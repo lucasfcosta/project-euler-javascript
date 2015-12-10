0c0ea4d1a4ba77a0e6c2354a8a252f923675806b
lucasfcosta <fernandesdacostalucas@gmail.com>
2015-12-10 21:45:52 -0200

    Fix folder name for tests

--- test/testAnswers.js
+++ test/testAnswers.js
@@ -5,11 +5,11 @@ let assert = require('chai').assert;
 let answers = require('./answers');
 
 // This needs to be synchronous for Mocha to behaviour correctly
-let files = fs.readdirSync(__dirname + '/../exercises');
+let files = fs.readdirSync(__dirname + '/../solutions');
 
 for (let i = 1; i <= files.length; i++) {
     describe('Problem ' + i, () => {
-        let answer = require('../exercises/' + ('00' + i).slice(-3));
+        let answer = require('../solutions/' + ('00' + i).slice(-3));
         
         it('Your answer: ' + answer, () => {
             let correctAnswer = answers['problem' + i];
