70d9cf18b842fb83b97826f056ddf04bee2f84d8
Lucas Fernandes da Costa <fernandesdacostalucas@gmail.com>
2015-12-16 09:20:17 -0200

    Efficient refactor of solution 9. Closes #4.

--- solutions/009.js
+++ solutions/009.js
@@ -3,17 +3,12 @@
 // Special Pythagorean triplet
 
 function calculateResult() {
-    for (let i = 0; i < 500; i++) {
-        let squareA = Math.pow(i, 2);
-        for (let j = 0; j < 500; j++) {
-            let squareB = Math.pow(j, 2);
-            for (let k = 0; k < 500; k++) {
-                let squareC = Math.pow(k, 2);
+    for (let a = 1; a < 334; a++) {
+        for (let b = a; b < 1000 - a; b++) {
+            let c = 1000 - a - b;
 
-                if ((squareA + squareB) === squareC && (i + j + k) === 1000) {
-                    return i * j * k;
-                }
-
+            if ((Math.pow(a, 2) + Math.pow(b, 2)) === Math.pow(c, 2)) {
+                return a * b * c;
             }
         }
     }
