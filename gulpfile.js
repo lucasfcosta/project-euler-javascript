5c0398dfcc542f124c8dcd79ca58e10ef75e28b0
lucasfcosta <fernandesdacostalucas@gmail.com>
2016-02-04 17:12:00 -0200

    Using only ESLINT

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict';
 
 let gulp = require('gulp');
-let jscs = require('gulp-jscs');
 let eslint = require('gulp-eslint');
 let mocha = require('gulp-mocha');
 let excludeGitignore = require('gulp-exclude-gitignore');
@@ -16,22 +15,15 @@ gulp.task('mochaLast', () => {
         .pipe(mocha({reporter: 'spec', timeout: '60000'}));
 });
 
-gulp.task('jscs', () => {
-    return gulp.src('**/*.js')
-        .pipe(excludeGitignore())
-        .pipe(jscs.reporter())
-        .pipe(jscs.reporter('fail'));
-});
-
 gulp.task('eslint', () => {
     return gulp.src('**/*.js')
         .pipe(excludeGitignore())
         .pipe(eslint())
-        .pipe(eslint.formatEach('compact', process.stderr))
+        .pipe(eslint.formatEach())
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['mocha', 'jscs', 'eslint']);
-gulp.task('test', ['mocha', 'jscs', 'eslint']);
+gulp.task('default', ['mocha', 'eslint']);
+gulp.task('test', ['mocha', 'eslint']);
 gulp.task('testLast', ['mochaLast']);
-gulp.task('lint', ['jscs', 'eslint']);
+gulp.task('lint', ['eslint']);
